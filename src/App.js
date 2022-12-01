368ff9589490d641bd3dd16b79a55aa5e9601beb
pablaw <chulhwa7@gmail.com>
2022-12-01 11:15:03 +0900

    feat: 투두리스트 결과물 제출

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [state, setState] = useState("");
+
+  const eventHander = (e) => {
+    setState(e.target.value);
+  };
+
+  const addTodoHandler = (e) => {
+    const newTodo = {
+      id: new Date().getTime(),
+      body: state,
+    };
+    if (state !== "") {
+      setTodos([...todos, newTodo]);
+    }
+    setState("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layout">
+      <div className="inputGroup">
+        <input
+          id="body"
+          maxLength={30}
+          className="add-input"
+          placeholder="할 일을 입력해주세요 :)"
+          value={state}
+          onChange={eventHander}
+        />
+        <button className="add-button" onClick={addTodoHandler}>
+          추가하기
+        </button>
+      </div>
+      <h2 style={{ textAlign: "center" }}>Todo List</h2>
+      <div className="todo-list">
+        {todos.map((todo) => {
+          return (
+            <div className="todo-card" key={todo.id}>
+              {todo.body}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
